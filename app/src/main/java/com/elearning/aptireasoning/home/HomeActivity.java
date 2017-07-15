aaa303b4676e96c5fa6ee4a6a7c3c29f044177da
khulesh <khulesh.gupta0722@gmail.com>
2017-07-15 22:46:33 +0530

    initial commit

---
+++ app/src/main/java/com/elearning/aptireasoning/home/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.elearning.aptireasoning.home;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.elearning.aptireasoning.R;
+
+/**
+ * Created by Nitin on 15-Jul-17.
+ */
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
